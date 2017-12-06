bbc21de943825aff6b3d0e7f7feb8836be667390
Gorpeln <339332@163.com>
2017-12-06 18:11:33 +0800

    CustomShareUI

---
+++ CustomShareUI/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CustomShareUI
+//
+//  Created by chen on 2014/12/6.
+//  Copyright © 2014年 Gorpeln. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CustomShareUI/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CustomShareUI
+//
+//  Created by chen on 2014/12/6.
+//  Copyright © 2014年 Gorpeln. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
